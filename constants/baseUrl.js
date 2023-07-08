c5a5dcbab9bf70edb8d364d09451eeb07b7f86b7
Omar <omar.frontend@gmail.com>
2023-07-08 23:30:45 +0300

    bug fix price

--- constants/baseUrl.js
+++ constants/baseUrl.js
@@ -1,4 +1,4 @@
 // const baseUrl = "https://yumtruxbe.onrender.com/";
-const baseUrl = "http://192.168.201.155:8000/";
+const baseUrl = "http://192.168.1.90:8000/";
 const payPalBaseUrl = "https://api-m.sandbox.paypal.com/";
 export { baseUrl, payPalBaseUrl };
