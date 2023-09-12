113ab65701594e08f3744693b6e1f0cc68418f41
DhirajsGithub <borsedhiraj123@gmail.com>
2023-09-12 13:31:38 +0530

    redeployment

--- screens/BlankScreen.js
+++ screens/BlankScreen.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useDispatch } from "react-redux";
@@ -8,13 +8,16 @@ import { baseUrl } from "../constants/baseUrl";
 import { getUserDetailsHttp, getUserStatus } from "../utils/user-http-requests";
 
 const BlankScreen = () => {
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   }, []);
   const getUserData = async (userId) => {
+    setLoading(true);
     let res = await getUserDetailsHttp(userId);
+    setLoading(false);
     console.log(res);
     if (res.status === "success" && res.user) {
       dispatch(setUserDetails(res.user));
@@ -24,7 +27,9 @@ const BlankScreen = () => {
     return res;
   };
   const fetchUserStatus = async (userId) => {
+    setLoading(true);
     const res = await getUserStatus(userId);
+    setLoading(false);
     if ((res.status && res.status === "inactive") || !res.status) {
       navigation.navigate("login");
       return;
@@ -37,26 +42,37 @@ const BlankScreen = () => {
       fetchUserStatus(val.userId);
       if (val?.token) {
         try {
+          setLoading(true);
           let res = await getUserData(val.userId);
+          setLoading(false);
           if (res.status === "success" && res.user) {
             navigation.navigate("main");
+          } else {
+            navigation.navigate("login");
           }
         } catch (error) {
+          setLoading(false);
           console.log(error);
         }
       } else {
         navigation.navigate("login");
       }
     } catch (e) {
+      setLoading(false);
       console.log(e);
     }
   };
   useEffect(() => {
-    getData();
+    try {
+      getData();
+    } catch (error) {
+      setLoading(false);
+    }
   }, []);
   return (
     <View style={styles.container}>
-      <Text style={{ color: "white" }}>Loading...</Text>
+      {loading && <Text style={{ color: "white" }}>Loading...</Text>}
+      {!loading && <Text style={{ color: "white" }}>Blank Screen</Text>}
     </View>
   );
 };
